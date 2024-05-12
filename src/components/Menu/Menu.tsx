e23f8d85ed2ce65f871b16b3789f04ae1fa7346c
Brijesh Kumar <bk10895@gmail.com>
2024-05-12 16:25:16 +0530

    feat: update Menu component for better navigation
    - Import and implement Link from 'react-router-dom' for better navigation
    - Comment out 'Sign Up' path from navLinks array temporarily
    - Update design of Menu component for improved user interface
    - Replace 'a' tags with 'Link' for better routing and performance

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 
 interface NavLink {
@@ -8,23 +9,28 @@ interface NavLink {
 const navLinks: NavLink[] = [
   { path: '/', label: 'Home' },
   { path: '/signin', label: 'Sign In' },
-  { path: '/signup', label: 'Sign Up' },
+  // { path: '/signup', label: 'Sign Up' },
   { path: '/about', label: 'About' },
   { path: '/profile', label: 'Profile' },
 ];
 
 export const Menu: React.FC = () => {
   return (
-    <nav className='bg-gray-800 p-4'>
-      <ul className='flex flex-col md:flex-row justify-between md:space-x-4'>
-        {navLinks.map((link, index) => (
-          <li key={index} className='md:mb-0 mb-2'>
-            <a className='text-white' href={link.path}>
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <nav className='bg-gray-800'>
+      <div className='flex justify-between max-w-6xl mx-auto p-3 text-white text-xl'>
+        <Link to='/'>
+          <h1 className=' font-bold'>My App</h1>
+        </Link>
+        <ul className='flex flex-col md:flex-row gap-4 justify-between md:space-x-4'>
+          {navLinks.map((link, index) => (
+            <li key={index} className='md:mb-0 mb-2'>
+              <Link className='text-white' to={link.path}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
