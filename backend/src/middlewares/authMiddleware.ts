eec506175975a71f92169210becd0233d6a2d94a
Brijesh Kumar <bk10895@gmail.com>
2024-05-20 16:53:51 +0530

    feat: add authentication middleware and secure user routes
    - Create new  for session-based user authentication
    - Apply  middleware to user routes to secure endpoints
    - Update middleware exports for easier import
    - Modify  to remove redundant types directory

---
+++ backend/src/middlewares/authMiddleware.ts
@@ -0,0 +1,45 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { User } from '../types';
+import { prisma } from '../db';
+
+// Local augmentation of the Express Request type
+interface CustomRequest extends Request {
+  user?: User;
+}
+
+export const isAuthenticated = async (
+  req: CustomRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const sessionToken = req.cookies['BRIJESH-AUTH'];
+  if (!sessionToken) {
+    return res
+      .status(401)
+      .json({ message: 'Unauthorized - No session token provided' });
+  }
+
+  try {
+    const authRecord = await prisma.authRecord.findUnique({
+      where: { sessionToken },
+      include: {
+        user: {
+          include: { authentication: true },
+        },
+      },
+    });
+
+    if (!authRecord || !authRecord.user) {
+      return res.status(401).json({
+        message: 'Unauthorized - Session not valid or user not found',
+      });
+    }
+
+    req.user = authRecord.user;
+    next();
+  } catch (error) {
+    console.error('Authentication error:', error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,10 +1,11 @@
 import { createUser, getAllUsers, getUserById } from '../controllers';
 
 import { Router } from 'express';
+import { isAuthenticated } from '../middlewares';
 
 const router = Router();
 
-router.get('/', getAllUsers);
+router.get('/', isAuthenticated, getAllUsers);
 router.get('/:id', getUserById);
 
 router.post('/', createUser);
